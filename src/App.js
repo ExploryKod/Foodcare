8b7aa7d84eabb906acd7b626b0981ef26b3464a8
AMAURY <amauryfranssen@gmail.com>
2022-05-15 19:13:19 +0200

    feat: set firebase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from "./routes/home";
-import Navigation from "./routes/navigation";
-import Compo from "./routes/compo";
+
+
 
 const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Navigation/>}>
         <Route index element={<Home/>}/>
-        <Route path='compo' element={<Compo/>}/>
-      </Route>
     </Routes>
    
   );
