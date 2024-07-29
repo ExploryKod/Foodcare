fd940be0d2c5d06816bf8516c9b0d19fe8a4e2f8
Amaury Franssen <amauryfranssen@gmail.com>
2024-07-30 01:08:08 +0200

    feat(client): paiement - configure error to handle no key error --wip

--- client/src/routes/StripePayment.jsx
+++ client/src/routes/StripePayment.jsx
@@ -9,14 +9,14 @@ const stripe = loadStripe('pk_test_51PYk4pEM5RDezzKdAc0G3CC6HFef0aat4IPHIJH7qMUH
  
 const StripePayment = () => {
     const [clientSecret, setClientSecret] = useState(null);
-    const [displayError, setDisplayError] = useState("");
+    const [displayError, setDisplayError] = useState({message: "", isError: false});
  
     useEffect(() => {
         axios
             .post(`${process.env.REACT_APP_API_URL}/create-payment-intent`, {
                 items: [{ id: 1, name: "momos", amount: 2.00 }],
             })
-            .then((resp) => setClientSecret(resp.data.clientSecret))
+            .then((resp) => {setClientSecret(resp.data.clientSecret)})
             .catch(function (error) {
                 if (error.response) {
                   // The request was made and the server responded with a status code
@@ -24,17 +24,17 @@ const StripePayment = () => {
                   console.log(error.response.data);
                   console.log(error.response.status);
                   console.log(error.response.headers);
-                  setDisplayError(`le serveur de paiement est indisponible: ${error.response.status}`)
+                  setDisplayError({message:`Raison: ${error.response.status}`, isError: true})
                 } else if (error.request) {
                   // The request was made but no response was received
                   // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                   // http.ClientRequest in node.js
                   console.log(error.request);
-                  setDisplayError(`le serveur de paiement est indisponible: statut ${error.request.status}`)
+                  setDisplayError({message:`Raison: statut ${error.request.status}`, isError: true})
                 } else {
                   // Something happened in setting up the request that triggered an Error
                   console.log('Error', error.message);
-                  setDisplayError(`le serveur de paiement est indisponible: ${error.message}`)
+                  setDisplayError({message: `Raison ${error.message}`, isError: true})
                 }
                 console.log(error.config);
             });
@@ -44,19 +44,23 @@ const StripePayment = () => {
         clientSecret,
         theme: "stripe",
     };
+
+    console.log(displayError)
  
     return (
         clientSecret ? (
             <Elements stripe={stripe} options={options}>
-                {displayError.length ? 
+                {!displayError.isError ? 
                 (<PaymentForm></PaymentForm>) : 
                 (<div className="container-center-child container-center-child--column">
                     <p className="category-text">Le service de paiement est actuellement indisponible</p>
+                    <p className="category-text">{displayError.message}</p>
                     <Ban size={100} color={'red'} />
                     </div>)}
             </Elements>)
             : (<div className="container-center-child container-center-child--column">
                 <p className="category-text">La clé secréte n'a pas été renouvelé pour ce service : il est indisponible</p>
+                <p className="category-text">{displayError.message}</p>
                 <Ban size={100} color={'red'} />
                 </div>)
         )
