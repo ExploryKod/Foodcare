64e35ab0b1bb863dfac908a5f1906e19148e0f16
Amaury Franssen <amauryfranssen@gmail.com>
2024-08-13 15:25:28 +0200

    feat(header): add indice to header

--- client/src/routes/navigation.jsx
+++ client/src/routes/navigation.jsx
@@ -1,9 +1,8 @@
 import { Fragment } from 'react';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../components/cart-icon'
-import CartDropdown from '../components/cart-dropdown'
 
 import { CartContext } from '../context/cart.context';
 import logo from '../assets/img/logo.png';
@@ -11,13 +10,41 @@ import logo from '../assets/img/logo.png';
 
 const Navigation = () => {
 
+    const { cartItems } = useContext(CartContext);
+    const [total, setTotal] = useState(0);
+    const [totalQuality, setTotalQuality] = useState({points: 0, color: ''});
+    useContext(CartContext);
+
+    function sumArray(arr) {
+        var total = arr.reduce(function(acc, curr) {
+          return acc + curr;
+        }, 0);
+        return total;
+      }
+
+    useEffect(() => {
+        let totalPrice = 0;
+        let totalNutrition = [];
+        cartItems.forEach((cartItem) => {
+          const { product_price, quantity, category_id } = cartItem;
+          const itemPrice = parseFloat(product_price) * parseFloat(quantity);
+          totalNutrition.push(category_id);
+          totalPrice += itemPrice;
+        });
+        setTotal(totalPrice);
+        
+        const nutriPoints = sumArray([...new Set(totalNutrition)]);
+        setTotalQuality({points: nutriPoints, color: nutriPoints < 6 ? 'red' : 'green'});
+    }, [cartItems]);
+
     return (
         <Fragment>
             <div className='navigation'>
                 <div className='logo-container'>
                     <Link to="/"><img src={logo} alt='logo' className='logo'></img></Link>
                 </div>
-
+                
+            
                 <div className='nav-links-container'>
                     <Link className='nav-link' to='/'>
                         ACCUEIL
@@ -34,6 +61,12 @@ const Navigation = () => {
                     <CartIcon />
                 </div>
             </div>
+            {totalQuality.points > 0 ?
+            (<div className="container"> 
+                <div className="indice-container">
+                    <p className="category-text">Mon indice de repas équilibré: <span>{totalQuality.points}</span></p>)  
+                </div>
+            </div>): null}
 
             <Outlet />
 
