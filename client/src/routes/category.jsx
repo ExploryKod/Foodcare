6910537b48ad5ac40260a708b2adf0cae89f8804
Amaury Franssen <amauryfranssen@gmail.com>
2024-08-14 11:56:39 +0200

    feat(loading) : add some interesting info while waiting

--- client/src/routes/category.jsx
+++ client/src/routes/category.jsx
@@ -16,6 +16,12 @@ export const Category = () => {
             <div className='category-container product-container'>
                     <div style={{ minHeight:'200px', padding:'30px', display:'flex', flexDirection:'column', justifyContent:'space-between', borderRadius:'10px', color: '#000', backgroundColor:'transparent', position:'absolute', top:'100px', right:'50%', transform: 'translateX(50%)'}}>
                         <h2 className="category-text"> Produits en cours de changement ... </h2>
+                        <span className="category-text"> <span>Le saviez-vous ?</span> 
+                        </span>
+                        <p className='mb-4 category-text'>Au Cameroun mais aussi dans d'autres pays, il existe des "sac marmites" en tissu fait pour accueillir des marmites 
+                            bouillantes et permette de finir la cuisson des aliments. 
+                            En plus d'économiser de l'électricité, cela permet aussi de mieux préserver certains nutriments.</p>
+                            <a href="https://biocamer.wordpress.com/2018/05/21/cameroun-cuire-ses-repas-dans-le-sac-marmite/">Lire l'article source</a>
                         <Link className="button-container" to={'/shop'}>Revenir au magasin</Link>
                     </div>
                 </div>
